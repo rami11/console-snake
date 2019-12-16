cfce2bd910baf33e6f2f97e62c996e653b2494f0
Rami Serapian <rsn@Ramis-MacBook-Pro.local>
2019-12-16 18:07:14 -0500

    Fix run script does not run on mac

--- run_game.sh
+++ run_game.sh
@@ -1,7 +1,7 @@
 if [ ! -e script_out ]; then
 	mkdir script_out
 fi
-find | grep .java > source.txt
+find . | grep .java > source.txt
 javac @source.txt -d script_out/
 rm source.txt
 java -cp script_out/ core.GameStarter
