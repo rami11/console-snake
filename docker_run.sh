1381d41a7244e44aaaddbd891fbf8563f2564f9b
rsn <rami.serapian@gmail.com>
2020-04-08 18:41:37 -0400

    Add some echos to the docker run script

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # make sure you have docker installed on your system before running this script.
 
 # constants
@@ -6,8 +8,14 @@ image_name=rsn11/console_snake:latest
 container_name=console_snake
 
 # build image
+echo Building ${image_name}
+echo "***************************************"
 docker build -f ${docker_file} -t ${image_name} .
 
+echo
+
 # run image
+echo Running ${image_name}
+echo "***************************************"
 docker run --rm -it --name ${container_name} ${image_name} \
     /bin/sh run_game.sh
